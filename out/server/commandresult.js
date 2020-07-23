bb95f5fced77b50ce2b5fb136ca4c1e9a22ad872
TheRealKS <koen.sauren@gmail.com>
2020-07-23 19:17:17 +0200

    removed production dir

--- out/server/commandresult.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CommandResult = void 0;
-class CommandResult {
-}
-exports.CommandResult = CommandResult;
--- out/util/constants.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.INVALID_MESSAGE = void 0;
-exports.INVALID_MESSAGE = "\{\"type\":\"INVALID\"\}";
