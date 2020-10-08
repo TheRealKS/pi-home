9ad334bdc5f2720b01938f76ff5217561ec5e75b
TheRealKS <koen.sauren@gmail.com>
2020-10-08 20:08:47 +0200

    Redoing scheduling

---
+++ public/schedule/src/overview-list.ts
@@ -0,0 +1,11 @@
+import { LitElement, html } from '../node_modules/lit-element/lit-element';
+
+class OverviewList extends LitElement {
+    render() {
+      return html`
+        
+      `;
+    }
+}
+
+customElements.define("overview-list", OverviewList);
