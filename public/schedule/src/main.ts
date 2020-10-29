de1b56c23379bc7dde8049954617a28efbbe10e0
TheRealKS <koen.sauren@gmail.com>
2020-10-29 20:50:06 +0100

    elements for event details

--- public/schedule/src/main.ts
+++ public/schedule/src/main.ts
@@ -1,9 +1,16 @@
 import { fetchProgramme } from "./scheduleparser"
 import {OverviewList} from "./elements/overview-list";
+import { DetailsBase } from "./elements/details-base";
+import { CircleSelector } from "./elements/circle-selector";
 
 window.onload = () => {
     fetchProgramme("test").then(p => {
        let overview = new OverviewList(p);
        document.getElementById("overview").appendChild(overview); 
     });
+
+    let det = new DetailsBase("Schakelpunt Toevoegen");
+    let circles = new CircleSelector(["M", "A", "B"]);
+    det.appendChild(circles);
+    document.getElementById("details").appendChild(det);
 }
